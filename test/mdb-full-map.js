d80b31ed6f57d9bb8eb70ce0bd37edbb8e18d0dc
Irakli Gozalishvili <contact@gozala.io>
2024-10-15 12:58:04 -0700

    chore: update test case

--- test/mdb-full-map.js
+++ test/mdb-full-map.js
@@ -9,20 +9,30 @@ export const testLMDB = {
   'write many records': (assert) =>
     Task.spawn(function* () {
       const url = new URL(`./test-okra-limit`, import.meta.url)
+      const json = new URL(`./test-flat-file.json`, import.meta.url)
       yield* Task.wait(FS.rm(url, { recursive: true, force: true }))
-      const synopsys = yield* Service.open(url)
+      // yield* Task.wait(FS.rm(json, { recursive: true, force: true }))
+      const synopsys = yield* Service.open(url, {
+        mapSize: 3 * 1024 * 1024 * 1024,
+      })
 
       const size = 1
       let count = 1
+      // const file = yield* Task.wait(FS.open(json, 'a'))
+      // file.write(`[\n`)
       while (true) {
         console.log(`Importing ${count}...`)
+        const batch = [...generateBatch(count, size)]
         const { ok, error } = yield* Task.perform(
-          Service.transact(synopsys, [...generateBatch(count, size)])
+          Service.transact(synopsys, batch)
         ).result()
 
         if (error) {
+          // yield* Task.wait(file.write(`\n]\n`))
           console.error(error)
           break
+        } else {
+          // yield* Task.wait(file.write(JSON.stringify(batch, null, 2) + ',\n'))
         }
         count += size
       }
